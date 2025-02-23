cfe86e2e3d11d3fc73529275036cbccc194c2336
PamaliRodrigo <pamalibuwanaja@gmail.com>
2025-02-23 11:19:44 +0530

    added public

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex items-center">
-            <img src="newlogo.png" className="h-10 w-10 text-blue-600" />
+            <img src="public\newlogo.png" className="h-10 w-10 text-blue-600" />
             <Link to="/" className="ml-2 text-xl font-bold text-slate-800">Sanction Guard</Link>
           </div>
           <div className="hidden md:flex items-center space-x-8">
